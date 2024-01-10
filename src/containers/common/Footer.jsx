35402f9ebc6efd2aaf5ac0810dcf6e051b36614e
Chisom <ozimmanuel79@gmail.com>
2024-01-10 14:19:14 +0100

    Update Footer.jsx

--- src/containers/common/Footer.jsx
+++ src/containers/common/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <section className="flex justify-center my-10">
         <Typography variant="p.medium">Copyright © 2023 StartEase.</Typography>
         <Typography variant="p.medium">
-          &nbsp; Page by <Link href="https://twitter.com/8mmnuel">eight.</Link>{" "}
+          &nbsp; Page by eight.{" "}
         </Typography>
       </section>
     </>
